daa6e247d360c95cded69c572aefde6c5de0459e
Marcos Navarro <marcosok91@gmail.com>
2020-05-29 23:27:37 +0200

    Añadida llamada de prueba al API

--- AgeOfEmpiresIIData/Program.cs
+++ AgeOfEmpiresIIData/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AgeOfEmpiresIIData
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient HttpClient = new HttpClient();
+        private static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            HttpResponseMessage result = await HttpClient.GetAsync("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations");
+            Console.WriteLine(result.Content.ReadAsStringAsync().Result);
+
+
+            // HttpWebRequest request = (HttpWebRequest) WebRequest.Create("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations");
+            // request.Method = "POST";
+            // request.ContentType = "application/x-www-form-urlencoded";
         }
     }
 }
